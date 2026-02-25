92799f93039007a8fbc6803d6bd479280c5dbfd6
JoeDE88 <joediaz88@hotmail.com>
2026-02-25 23:56:54 +0100

    refactored strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,21 +14,29 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char		*str;
-	size_t		len;
-	size_t		s1_len;
-	size_t		s2_len;
+	char	*str;
+	size_t	len;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	len = s1_len + s2_len;
+	len = ft_strlen(s1) + ft_strlen(s2);
 	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	ft_memcpy(str, s1, s1_len);
-	ft_memcpy(str + s1_len, s2, s2_len);
-	str[len] = '\0';
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
 	return (str);
 }
